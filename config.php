a9ecfd74bf26e44909eaf26e92d195b3457ccedf
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-04-10 06:51:30 +0200

    add url rules for pretty url

--- config.php
+++ config.php
@@ -9,6 +9,11 @@ return [
     'id' => 'chat',
     'class' => 'humhub\modules\chat\Module',
     'namespace' => 'humhub\modules\chat',
+    'urlManagerRules' => [
+        'chat/<controller:[\w-]+>' => 'chat/<controller>',
+        'GET chat/<controller:[\w-]+>/get-rooms' => 'chat/<controller>/get-rooms',
+        'chat/<controller:[\w-]+>/<url:[a-zA-Z0-9-]+>' => 'chat/<controller>',
+    ],
     'events' => [
         ['class' => TopMenu::className(), 'event' => TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\chat\Events', 'onTopMenuInit']],
         ['class' => NotificationArea::className(), 'event' => NotificationArea::EVENT_INIT, 'callback' => ['humhub\modules\chat\Events', 'onNotificationAddonInit']],
